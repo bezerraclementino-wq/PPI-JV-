15b19fff916a27831ee7038950d3b59185b4320b
joao vitor bezerra clementino <20221134010044@ifrn.local>
2025-12-04 11:36:18 -0300

    subi login funcionando

--- login/app/viws/painel.php
+++
@@ -1,3 +0,0 @@
-<h2>Bem-vindo, <?= $_SESSION['usuario'] ?></h2>
-
-<a href="index.php?action=logout">Sair</a>
--- login/index.php
+++ login/index.php
@@ -1,55 +1,24 @@
 <?php
-include('conexao.php');
-if (isset($_POST['email']) || isset( $_POST['senha'])) {
-    if (strlen($_POST['email']) ==0 ) {
-        echo "Preencher seu Email";
-    }
-    else if (strlen($_POST["senha"]) == 0) {
-        echo "Preencha sua Senha";
-    }else {
-        $email = $mysqli->real_escape_string($_POST["email"]);
-        $senha = $mysqli->real_escape_string($_POST["senha"]);
+require_once __DIR__ . '/controllers/AuthController.php';
 
-        $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha ='$senha' ";
-          $listar_usuario = $mysqli->query($sql);
-          $quantidade = $listar_usuario->num_rows;
-          if ($quantidade == 1) {
-                $usuario = $listar_usuario->fetch_assoc();
-                if (!isset ($_SESSION)) {
-                    session_start();
-                }
-                $_SESSION['id'] = $usuario['id'];
-                $_SESSION['nome'] = $usuario['nome'];
-                header("location: painel.php");
-          }else {
-            echo"Falha ao Logar. ";
-          }
-
-    }
+if (!isset($_SESSION)) {
+    session_start();
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h1>Acesse sua conta</h1>
-    <form action="" method="POST">
-        <p>
-            <label>E-mail</label>
-            <input type="text" name="email">
-        </p>
-        <p>
-            <label>Senha</label>
-            <input type="password" name="senha">
-        </p>
-        <p>
-            <button type="submit">Entrar</button>
-        </p>
-    </form>
-</body>
-</html>
+
+$controller = new AuthController();
+
+$action = $_GET['action'] ?? 'index';
+
+switch ($action) {
+    case 'logar':
+        $controller->logar();
+        break;
+
+    case 'painel':
+        $controller->painel();
+        break;
+
+    default:
+        $controller->index();
+        break;
+}
